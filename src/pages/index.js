5b7a3500088251b48b0fd7d047b51a4af32fa963
Jamieson Reinhard <jamieson@zoee.com>
2023-06-19 20:01:41 -0600

    can't find layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import LargeInput from "../components/largeInput";
 export default function Home() {
   return (
     <Layout>
-      <div className="mx-auto w-[90%] max-w-[1200px] py-[100px] text-center flex flex-col items-center gap-[32px]">
+      <div className="mx-auto w-[90%] max-w-[1200px] py-[120px] text-center flex flex-col items-center gap-[32px]">
         <h1 className="text-[40px] font-bold"><em>Flashcards AI</em></h1>
         <LargeInput />
       </div>
